5ef35e0bc264db84a118dbe5d65daf7ddb8820ae
mariag5490 <92363619+mariag5490@users.noreply.github.com>
2021-11-28 16:13:34 +0530

    Initial commit

--- StreamingWC.py
+++ StreamingWC.py
@@ -1,4 +1,5 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import expr
 
 from lib.logger import Log4j
 
@@ -7,8 +8,27 @@ if __name__=="__main__":
         .builder \
         .appName('Streaming Word Count') \
         .master('local[3]') \
+        .config("spark.streaming.stopGracefullyOnShutdown","true") \
+        .config("spark.sql.shuffle.partitions",3) \
         .getOrCreate()
 
     logger = Log4j(spark)
+
+    linesDF = spark.readStream \
+            .format("socket") \
+            .option("host","localhost") \
+            .option("port","9999") \
+            .load()
+
+    wordsDf = linesDF.select(expr("explode(split(value,'    ')) as word"))
+    wordCount = wordsDf.groupBy("word").count()
+
+    countsDf = wordCount.writeStream \
+            .format("console") \
+            .option("checkPointLocation","chk-point-dir") \
+            .outputMode("complete") \
+            .start()
+
+    countsDf.awaitTermination()
 
 
